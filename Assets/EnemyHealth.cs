3332343fde34a2d209fdce2755c1fd9b9f67373c
JanuchowskiDawid <mmalpa97@wp.pl>
2022-01-20 20:35:41 +0100

    Added particle system and enemy hp

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 5;
+    int currentHitPoints;
+    private void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        TakeDamage();         
+    }
+
+    private void TakeDamage()
+    {
+        currentHitPoints--;
+        if (currentHitPoints<=0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
